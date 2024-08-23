da06ab55f8910c051f09627a01118f443d9336ae
Meas Manet <mrneth58@gmail.com>
2024-08-23 10:05:10 +0700

    add cors

--- app/main.py
+++ app/main.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI
 from app.routes import category
+from fastapi.middleware.cors import CORSMiddleware
 
 def create_application():
     application = FastAPI()
     application.include_router(category.category_router)
     return application
 
+origins = [
+    "http://localhost:3000", 
+]
+
 
 app = create_application()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows requests from these origins
+    allow_credentials=True,  # Allows cookies to be sent in cross-origin requests
+    allow_methods=["*"],  # Allows all HTTP methods
+    allow_headers=["*"],  # Allows all headers to be sent in requests
+)
 
 
 @app.get("/")
